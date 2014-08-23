5ad1d960305cfd666fcfc30cfd42659f9f62a0e8
Dan Gamble <dan.gamble@onespacemedia.com>
2014-08-23 16:03:56 +0100

    Added templates folder with some base templates

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,11 +1,25 @@
 # Core imporst
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^$', TemplateView.as_view(template_name='base.html')),
+    url(
+        r'^$',
+        TemplateView.as_view(template_name='base.html'),
+        name='homepage'
+        ),
     url(r'^admin/', include(admin.site.urls)),
-)
+    # url(r'^/app/', include('apps.app.urls', namespace='app')),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        '',
+        url("^404/$", TemplateView.as_view(template_name="404.html")),
+        url("^500/$", TemplateView.as_view(template_name="500.html")),
+    )
